203548e0de19d3adc47edb8adc0bf0bd014001de
Brianojunga <brianojunga3@gmail.com>
2025-03-28 21:58:21 +0300

    Add core functionality amd implement Promise.all

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import DisplayPokemon from "./pokemon";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=12`, { mode: "cors" })
+      .then((response) => {
+        if (response >= 400) throw new Error("server error");
+        return response.json();
+      })
+      .then((response) => {
+        const pokemonsPromises = response.results.map((pokemon) =>
+          fetch(pokemon.url).then((response) => response.json()),
+        );
+
+        Promise.all(pokemonsPromises).then((response) => {
+          const sortedResponse = response.map((pokemon) => {
+            return {
+              id: crypto.randomUUID(),
+              name: pokemon.name,
+              url: pokemon.sprites.other["official-artwork"].front_default,
+            };
+          });
+          setPokemons(sortedResponse);
+        });
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Network error was encountered</p>;
+
+  
   return (
-    <>
-    </>
-  )
+    <DisplayPokemon pokemons={pokemons} setFunction={setPokemons}/>
+  );
 }
 
-export default App
+export default App;
